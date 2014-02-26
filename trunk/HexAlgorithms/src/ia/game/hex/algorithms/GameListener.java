8aa512e375cd436844dd813b9f124e8ed3e5e266
napicella4@gmail.com <napicella4@gmail.com@5c1efd8e-bdbf-146a-9845-c6e7390b4e02>
2014-02-26 15:53:06 +0000

    ciao!

--- trunk/HexAlgorithms/src/ia/game/hex/algorithms/GameListener.java
+++
@@ -1,5 +0,0 @@
-package ia.game.hex.algorithms;
-
-public interface GameListener {
-	public void update(Player args);
-}
--- trunk/HexAlgorithms/src/ia/game/hex/algorithms/InvalidPlacementException.java
+++
@@ -1,5 +0,0 @@
-package ia.game.hex.algorithms;
-
-public class InvalidPlacementException extends Exception {
-
-}
